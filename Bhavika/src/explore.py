fbdfab10fc154c21a37e844a40645b6743b02267
Bhavika Tekwani <bhavicka.7992@gmail.com>
2017-09-26 23:58:20 -0400

    Finding Impressionists

--- Bhavika/src/explore.py
+++ Bhavika/src/explore.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from PIL import Image
+import os
 
 base_address = '/home/bhavika/wikiart/'
 
@@ -8,14 +9,30 @@ def read_artist_data(base_address, filepath):
     filepath = base_address + filepath
     paintings_by_artist = pd.read_csv(filepath,names=['location', 'class'], header=0)
     paintings_by_artist['absolute_location'] = base_address+paintings_by_artist['location']
-    collage = Image.new('RGB', (800, 800))
+    paintings_by_artist = paintings_by_artist.sort_values('class')
     for i in range(paintings_by_artist.shape[0]):
         link = paintings_by_artist.iloc[i]['absolute_location']
         print(link)
-    # This will only open the last image - only to test.
     img = Image.open(link)
     img.show()
+
+
+def select_impressionist_artists(filepath):
+    paintings = os.listdir(filepath)
+    unique_artists = set()
+    artist_paintings_count = {}
+    for filename in paintings:
+        s = filename.find('_')
+        artist_name = filename[:s]
+        unique_artists.add(artist_name)
+        if artist_name in artist_paintings_count.keys():
+            artist_paintings_count[artist_name] += 1
+        else:
+            artist_paintings_count[artist_name] = 1
+    print("Total Unique Artists in the Impressionism style:", len(unique_artists))
+    print("Paintings by each artist:", artist_paintings_count)
 
 if __name__ == '__main__':
     train_file = '/artist_train.csv'
     read_artist_data(base_address=base_address,filepath=train_file)
+    select_impressionist_artists(base_address+'Impressionism')
