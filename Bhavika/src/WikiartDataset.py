f3d495dd6b96632ca10b36915eabc26ce2f6dbc6
Bhavika Tekwani <bhavicka.7992@gmail.com>
2017-10-05 22:33:20 -0400

    Optimized imports, Dataset label fixes

--- Bhavika/src/WikiartDataset.py
+++ Bhavika/src/WikiartDataset.py
@@ -1,19 +1,39 @@
 import torch.utils.data as data_utils
-import os
 import torch
-import torchvision
-from torchvision import transforms
-import random
 from PIL import Image
 import pandas as pd
 import numpy as np
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
 class WikiartDataset(data_utils.Dataset):
     def __init__(self, config):
         self.dataset_path = config.get('wikiart_path')
         self.images = config.get('images_path')
-        self.num_samples = config.get('train_size')
+        self.num_samples = config.get('size')
         self.ids_list = list(range(1, self.num_samples+1))
         # random.shuffle(self.ids_list)
 
@@ -24,17 +44,34 @@ class WikiartDataset(data_utils.Dataset):
         image = image.resize((300, 300))
         image = np.array(image).astype(np.float32)
         label = row['class']
+
         sample = {'image': torch.from_numpy(image), 'class': label}
         return sample
 
     def __len__(self):
         return len(self.ids_list)
 
-wiki = WikiartDataset(config={'wikiart_path': '../data/impressionists.csv',
+
+wiki_train = WikiartDataset(config={'wikiart_path': '../data/impressionists.csv',
                               'images_path': '/home/bhavika/wikiart/Impressionism',
-                              'train_size': 500})
+                              'size': 500})
 
-dataloader = data_utils.DataLoader(wiki, batch_size=4, shuffle=True, num_workers=2)
+wiki_test = WikiartDataset(config={'wikiart_path': '../data/impressionists.csv',
+                              'images_path': '/home/bhavika/wikiart/Impressionism',
+                              'size': 500})
 
-for i, sample in enumerate(dataloader):
-    print (i, sample['class'], sample['image'].size())
+wiki_train_dataloader = data_utils.DataLoader(wiki_train, batch_size=4, shuffle=True, num_workers=2)
+wiki_test_dataloader = data_utils.DataLoader(wiki_test, batch_size=4, shuffle=True, num_workers=2)
+
+net = Net()
+
+# Defining the loss function
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+def get_classes(filepath):
+    data = pd.read_csv(filepath, sep=',')
+    return list(data[0:1000]['class'].unique())
+
+classes = get_classes('../data/impressionists.csv')
