f317f76167c93be6012fd4881f0791e9a9f21dab
Bhavika Tekwani <bhavicka.7992@gmail.com>
2017-11-26 19:31:37 -0500

    Fixed graphviz network visualizations

--- Bhavika/viz/viz_resnet.py
+++ Bhavika/viz/viz_resnet.py
@@ -2,9 +2,23 @@ from graphviz import Digraph
 import torch
 from torch.autograd import Variable
 import torchvision.models as models
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-def make_dot(var):
+def make_dot(var, params=None):
+    """ Produces Graphviz representation of PyTorch autograd graph
+    Blue nodes are the Variables that require grad, orange are Tensors
+    saved for backward in torch.autograd.Function
+    Args:
+        var: output Variable
+        params: dict of (name, Variable) to add names to node that
+            require grad (TODO: make optional)
+    """
+    if params is not None:
+        assert isinstance(params.values()[0], Variable)
+        param_map = {id(v): k for k, v in params.items()}
+
     node_attr = dict(style='filled',
                      shape='box',
                      align='left',
@@ -14,25 +28,65 @@ def make_dot(var):
     dot = Digraph(node_attr=node_attr, graph_attr=dict(size="12,12"))
     seen = set()
 
+    def size_to_str(size):
+        return '('+(', ').join(['%d' % v for v in size])+')'
+
     def add_nodes(var):
         if var not in seen:
-            if isinstance(var, Variable):
-                value = '('+(', ').join(['%d'% v for v in var.size()])+')'
-                dot.node(str(id(var)), str(value), fillcolor='lightblue')
+            if torch.is_tensor(var):
+                dot.node(str(id(var)), size_to_str(var.size()), fillcolor='orange')
+            elif hasattr(var, 'variable'):
+                u = var.variable
+                name = param_map[id(u)] if params is not None else ''
+                node_name = '%s\n %s' % (name, size_to_str(u.size()))
+                dot.node(str(id(var)), node_name, fillcolor='lightblue')
             else:
                 dot.node(str(id(var)), str(type(var).__name__))
             seen.add(var)
-            if hasattr(var, 'previous_functions'):
-                for u in var.previous_functions:
-                    dot.edge(str(id(u[0])), str(id(var)))
-                    add_nodes(u[0])
+            if hasattr(var, 'next_functions'):
+                for u in var.next_functions:
+                    if u[0] is not None:
+                        dot.edge(str(id(u[0])), str(id(var)))
+                        add_nodes(u[0])
+            if hasattr(var, 'saved_tensors'):
+                for t in var.saved_tensors:
+                    dot.edge(str(id(t)), str(id(var)))
+                    add_nodes(t)
     add_nodes(var.grad_fn)
     return dot
 
+# inputs = torch.randn(4,3,224,224)
+# resnet18 = models.resnet18(pretrained=True)
+# y = resnet18(Variable(inputs))
+#
+#
+# g = make_dot(y)
+# g.view()
 
-inputs = torch.randn(4,3,224,224)
-resnet18 = models.resnet18(pretrained=True)
-y = resnet18(Variable(inputs))
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 15)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+inputs = torch.randn(4, 3, 32, 32)
+cnn = Net()
+y = cnn(Variable(inputs))
 
 
 g = make_dot(y)
