4e3845d501190e1e39fb1f2bbb09ac7006b657da
Melanie <mbakerf@gmu.edu>
2017-11-28 11:30:26 -0500

    plot images

--- Melanie/src/DEMO_PredictArtist_RandImg.py
+++ Melanie/src/DEMO_PredictArtist_RandImg.py
@@ -8,6 +8,9 @@ Created on Sun Nov 26 23:49:34 2017
 import DEMO_LoadDataset_PredictOnly
 from keras.models import load_model
 import constants
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
 
 # returns a compiled model
 # identical to the previous one
@@ -23,6 +26,27 @@ print("Dataset is loaded.")
 modelPredictions = model.predict(x_predict)
 
 print("Got predictions.")
+
+pissarro_Img1 = mpimg.imread(constants.small_img_path +'camille-pissarro_apple-trees-and-poplars-in-the-setting-sun-1901.jpg')
+pissarro_Img2 = mpimg.imread(constants.small_img_path +'camille-pissarro_avenue-de-l-opera-effect-of-snow-1898.jpg')
+
+hassam_Img1 = mpimg.imread(constants.small_img_path +'childe-hassam_across-the-avenue-in-sunlight-june.jpg')
+hassam_Img2 = mpimg.imread(constants.small_img_path +'childe-hassam_an-evening-street-scene-pont-aven.jpg')
+
+monet_Img1 = mpimg.imread(constants.small_img_path +'claude-monet_andre-lauvray(1).jpg')
+monet_Img2 = mpimg.imread(constants.small_img_path +'claude-monet_apple-trees-in-bloom-at-giverny-1901(1).jpg')
+
+plt.subplot(3,3,1), plt.imshow(x_predict[0])
+plt.subplot(3,3,2), plt.imshow(x_predict[1])
+plt.subplot(3,3,3), plt.imshow(x_predict[2])
+
+plt.subplot(3,3,4), plt.imshow(pissarro_Img1)
+plt.subplot(3,3,5), plt.imshow(hassam_Img1)
+plt.subplot(3,3,6), plt.imshow(monet_Img1)
+
+plt.subplot(3,3,7), plt.imshow(pissarro_Img2)
+plt.subplot(3,3,8), plt.imshow(hassam_Img2)
+plt.subplot(3,3,9), plt.imshow(monet_Img2)
 
 baltatuCorrect = 0
 sisleyCorrect = 0
