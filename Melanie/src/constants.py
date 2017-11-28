07808ebd1ec59bf15fb2ac21379dd8cb6be1d80e
Melanie <mbakerf@gmu.edu>
2017-11-28 11:31:53 -0500

    Add files via upload

--- Melanie/src/constants.py
+++ Melanie/src/constants.py
@@ -6,13 +6,15 @@ Created on Fri Nov 24 01:01:25 2017
 """
 
 #Prep_dataset.py 
-csv = 'C:\\Users\\Melanie\\Documents\\GitHub\\Frida\\Bhavika\\data\\top15.csv'
+csv = 'C:\\Users\\Melanie\\Desktop\\csvFiles\\data\\top15.csv'
+#csv = 'C:\\Users\\Melanie\\Documents\\GitHub\\Frida\\Bhavika\\data\\top15.csv'
 wiki_art_path = 'C:\\Users\\Melanie\\Desktop\\WikiartData\\wikiart\\Impressionism\\'
 original_img_path = 'C:\\Users\\Melanie\\Desktop\\impress_data'
 small_img_path = 'C:\\Users\\Melanie\\Desktop\\impress_data_small\\'
 
 #LoadDataset.py
-img_count = 600
+img_count = 750
+#img_count = 600
 
 #CNN.py
 bs = 15
@@ -21,8 +23,8 @@ epochs = 100
 model_folder = 'C:/KerasModels/Model'
 
 #PredictArtist.py
-model_path = 'C:\KerasModels\Model2017-11-24 17-15-06\ArtistIdentificationModel.h5'
-test_samples = 4
+model_path = 'C:\KerasModels\Model2017-11-27 23-00-27\ArtistIdentificationModel.h5'
+test_samples = 16
 
 #LoadDataset_predictonly
 demo_path = 'C:\\Users\\Melanie\\Desktop\\randImg\\'
